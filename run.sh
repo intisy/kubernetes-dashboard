66f823e07bf21c907f0414cbc73ffe7ffcfefdc6
Pizza <wilderpizza99@gmail.com>
2024-07-18 13:36:36 +0200

    fixed runner

--- run.sh
+++ run.sh
@@ -4,8 +4,8 @@ action=$1
 
 execute() {
   substring="#!/bin/bash"
-  sha=$(curl -sSL https://api.github.com/repos/WildePizza/kubernetes-dashboard/commits?per_page=2 | jq -r '.[1].sha')
-  url="https://raw.githubusercontent.com/WildePizza/kubernetes-dashboard/HEAD/.commits/$sha/scripts/$action.sh"
+  sha=$(curl -sSL https://api.github.com/repos/WildePizza/kubernetes-dashboard/commits | jq -r '.[1].sha')
+  url="https://raw.githubusercontent.com/WildePizza/kubernetes-dashboard/$sha/scripts/$action.sh"
   echo "Executing: $url"
   output=$(curl -fsSL $url 2>&1)
   if [[ $output =~ $substring ]]; then
